4d9cd87561f6c5209e19f9b5a6154a87e708b57a
mariamtamerr <memariam5@outlook.com>
2023-10-25 01:02:15 +0300

    styling update/delete tweets

--- twitter/routes.py
+++ twitter/routes.py
@@ -2,7 +2,7 @@
 
 
 
-from flask import render_template, url_for, redirect, flash, session, abort
+from flask import render_template, url_for, redirect, flash, session, abort, request
 from twitter import app, db , bcrypt  
 from twitter.forms import RegistrationForm, LoginForm, PostForm
 from twitter.models import User, Post
@@ -102,7 +102,7 @@ def new_post():
         db.session.commit()
         flash('You tweet has been sent', 'primary')
         return redirect(url_for('home'))
-    return render_template('create_post.html', title='New Post', form=form)
+    return render_template('create_post.html', title='New Post', form=form, legend='New Post')
 
 
 # a route that takes us a specific page of a specific tweet
@@ -122,15 +122,23 @@ def update_post(post_id):
         abort(403)
     form = PostForm() #make an instance of the form
     if form.validate_on_submit():
-        # if current_user.is_authenticated:
-        post = Post(title=form.title.data, content=form.content.data, author=current_user)
-        db.session.add(post)
-        db.session.commit()
-        return redirect(url_for('home'))
-    return render_template('create_post.html', title='New Post', form=form)
+        post.title = form.title.data
+        post.content = form.content.data
+        # post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        # db.session.add(post) we don't need it bc they're already in the db 
+        db.session.commit() #we just wanna update / commit them
+        flash('Your Tweet Has Been Updated', 'success')
+        # return redirect(url_for('home'))
+        form.title.data = post.title # so when i open the update page , i can find the current title and content
+        form.content.data = post.content #############
+        return redirect(url_for('post', post_id=post.id)) #return for that specific id post
+    elif request.method=='GET':
+        form.title.data = post.title # so when i finish updating, i can see my changes aka current new title and content
+        form.content.data = post.content #############
+    return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')
 
 
-@app.route('/post/<int:post_id>/delete', methods=['GET','POST'])
+@app.route('/post/<int:post_id>/delete', methods=['POST']) # POST request only received from the modal
 @login_required 
 def delete_post(post_id):
     post = Post.query.get_or_404(post_id)
@@ -138,9 +146,8 @@ def delete_post(post_id):
         abort(403)
     db.session.delete(post)
     db.session.commit()
+    flash('Your Tweet Has Been Deleted', 'success')
     return redirect(url_for('home'))
-    # return render_template('create_post.html', title='New Post', form=form)
-
-
+  
     
 
